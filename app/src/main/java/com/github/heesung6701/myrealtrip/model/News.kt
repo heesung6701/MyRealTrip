f49f3727bcfe0c71f29193f7715f338516e8a725
윤희성 <heesung6701@naver.com>
2020-03-19 22:53:42 +0900

    add keyword's algorithm

--- app/src/main/java/com/github/heesung6701/myrealtrip/model/News.kt
+++ app/src/main/java/com/github/heesung6701/myrealtrip/model/News.kt
@@ -1,56 +1,32 @@
 package com.github.heesung6701.myrealtrip.model
 
-import android.os.Parcel
-import android.os.Parcelable
 import android.util.Log
 import java.util.*
+import kotlin.collections.ArrayList
 
-data class News(val title: String, val content: String, val thumbnail: String) : Parcelable{
+data class News(val link: String, val title: String, val content: String, val thumbnail: String){
+    var keywords: List<String>
+
     init {
-        updateKeyword(content)
-    }
-    var keywords: List<String>? = null
-
-    private fun updateKeyword(content: String){
-        val items = content.split(" ").fold(HashMap<String, Int>()){ acc, str ->
-            acc[str]?.plus(1)?: acc.put(str, 1)
-            return
+        val items = content.split(" ").fold(HashMap<String, Int>()) { acc, str ->
+            acc[str]?.plus(1) ?: acc.put(str, 1)
+            return@fold acc
         }
-        /*val queue = content.split(" ").fold(PriorityQueue<Pair<String, Int>>()) { q : PriorityQueue<Pair<String, Int>>, str ->
-            val cnt = q.
-            q.put(Pair(str, )
-            q.put(str)
-        }*/
->>>>>>> 2bef209... temp
-        keywords = listOf("dummy1", "dummy2", "dummy3")
-    }
-
-    override fun writeToParcel(dest: Parcel, flags: Int) {
-        dest.writeString(thumbnail)
-        dest.writeString(title)
-        dest.writeString(content)
-    }
-
-    constructor(parcel: Parcel) : this(
-        parcel.readString()!!,
-        parcel.readString()!!,
-        parcel.readString()!!
-    ) {
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<News> {
-        const val INTENT_NAME: String = "News"
-
-        override fun createFromParcel(parcel: Parcel): News {
-            return News(parcel)
+        val pq = PriorityQueue<Pair<String, Int>>(items.size, compareBy {
+            -it.second
+        })
+        for ((k, v) in items) {
+            pq.add(Pair<String, Int>(k, v))
         }
-
-        override fun newArray(size: Int): Array<News?> {
-            return arrayOfNulls(size)
+        val list: ArrayList<String> = ArrayList<String>(3)
+        for (i in 1..3) {
+            if (pq.isEmpty()) {
+                list.add("")
+                continue
+            }
+            list.add(pq.poll().first)
         }
+        keywords = list.toList()
+        Log.e("~~~~", keywords.toString())
     }
 }
