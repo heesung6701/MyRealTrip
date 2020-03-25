e509c3f5289df1a218134c97d16640d5afb34f07
Heesung Youn <heesung6701@naver.com>
2020-03-25 22:11:37 +0900

    remove mock data

--- app/src/main/java/com/github/heesung6701/myrealtrip/main/MainActivity.kt
+++ app/src/main/java/com/github/heesung6701/myrealtrip/main/MainActivity.kt
@@ -6,19 +6,14 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.github.heesung6701.myrealtrip.R
 import com.github.heesung6701.myrealtrip.main.adapter.NewsListAdapter
 import com.github.heesung6701.myrealtrip.model.News
+import com.github.heesung6701.myrealtrip.network.NetworkHelper
 import com.github.heesung6701.myrealtrip.repository.NewsRepository
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
 
-    private val list = mutableListOf<News>(
-        News(thumbnail = "https://pbs.twimg.com/profile_images/870563578704613376/8YPGkZbY.jpg",title = "title1",content = "content1"),
-        News(thumbnail = "https://s.pstatic.net/static/www/mobile/edit/2020/0316/cropImg_166x108_26294546710170353.jpeg",title = "title2",content = "content2"),
-        News(thumbnail = "https://s.pstatic.net/static/www/mobile/edit/2020/0316/cropImg_166x108_26265426473432757.jpeg",title = "title3",content = "content3"),
-        News(thumbnail = "https://pbs.twimg.com/profile_images/870563578704613376/8YPGkZbY.jpg",title = "title4",content = "content4"),
-        News(thumbnail = "https://pbs.twimg.com/profile_images/870563578704613376/8YPGkZbY.jpg",title = "title5",content = "content5"),
-        News(thumbnail = "https://pbs.twimg.com/profile_images/870563578704613376/8YPGkZbY.jpg",title = "title6",content = "content6"))
+    private val list = mutableListOf<News>()
 
     private var adapter : NewsListAdapter by Delegates.notNull()
     private val newsRepository = NewsRepository()
