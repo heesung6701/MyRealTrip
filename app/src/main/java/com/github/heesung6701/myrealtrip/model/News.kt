a5979ecd75ef2bc85c7d0ac09d7200f80857b467
윤희성 <heesung6701@naver.com>
2020-03-29 10:48:43 +0900

    fix extract keyword algorithm

--- app/src/main/java/com/github/heesung6701/myrealtrip/model/News.kt
+++ app/src/main/java/com/github/heesung6701/myrealtrip/model/News.kt
@@ -8,13 +8,17 @@ data class News(val link: String, val title: String){
 
     var content: String? = null
     set(value) {
-        value?.let { updateKeyword(it) }
+        value?.let {
+            if(it.isBlank()) return@let
+            updateKeyword(it)
+        }
         field = value
     }
     var thumbnail: String? = null
 
     private fun updateKeyword(content: String){
-        val items = content.split("[^ㄱ-ㅎ가-힣ㅏ-ㅣa-zA-Z0-9]".toRegex()).fold(HashMap<String, Int>()) { acc, str ->
+        val items = content.split("[^ㄱ-ㅎ^가-힣ㅏ-ㅣa-zA-Z0-9]".toRegex()).fold(HashMap<String, Int>()) { acc, str ->
+            if(str=="") return@fold acc
             acc[str]?.plus(1) ?: acc.put(str, 1)
             return@fold acc
         }
