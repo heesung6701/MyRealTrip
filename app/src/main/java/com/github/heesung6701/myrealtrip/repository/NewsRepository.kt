cb90e5c890f7b599f7f32f0b1c1cb6c949c0b56f
윤희성 <heesung6701@naver.com>
2020-03-29 12:41:47 +0900

    separate subscribe to map. flatMapIterable, subscribe

--- app/src/main/java/com/github/heesung6701/myrealtrip/repository/NewsRepository.kt
+++ app/src/main/java/com/github/heesung6701/myrealtrip/repository/NewsRepository.kt
@@ -17,23 +17,25 @@ class NewsRepository {
         return NetworkHelper.apiService.getNewsList()
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
-            .subscribe { rss ->
-                val channel = rss.channel ?: return@subscribe
-                val news = channel.news ?: return@subscribe
-                val col = news.map {  News(link = it.link.toString(),title = it.title.toString()) }
-                col.forEach {
-                    val link = it.link
-                    GlobalScope.launch(Dispatchers.Main) {
-                        val list = getOGTag(link)
-                        if(list?.size != 2) return@launch
-                        with(it) {
-                            content = list[0]
-                            thumbnail = list[1]
-                        }
-                        onUpdate()
-                    }
-                }
+            .map { rss ->
+                val channel = rss.channel ?: throw Exception("Parsing Error: channel is not found on rss")
+                val news = channel.news ?: throw Exception("Parsing Error: news is not found on channel")
+                val col = news.map {News(link = it.link.toString(), title = it.title.toString())}
                 onFinish(col)
+                return@map col
+            }
+            .flatMapIterable { list -> list }
+            .subscribe { news->
+                val link = news.link
+                GlobalScope.launch(Dispatchers.Main) {
+                    val list = getOGTag(link)
+                    if(list?.size != 2) return@launch
+                    with(news) {
+                        content = list[0]
+                        thumbnail = list[1]
+                    }
+                    onUpdate()
+                }
             }
     }
     private suspend fun getOGTag(url: String): List<String>?{
